c4c3cc02e4cd47a3ec8b80c30d338174d20623e0
Agung Gemilang <gemilanga163@gmail.com>
2021-08-28 02:24:00 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 su root
 apt-get update && apt-get install sudo -y
-sudo apt-get install nmp -y
-sudo su
+sudo apt-get install build-essential libssl-dev -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
 git clone https://gitlab.com/gemilanga163/onta.git
 cd onta
 chmod u+x gas

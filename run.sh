a9fc1db4c8583794d2af3f401e1a9388545f5b57
01057057 <112078331+01057057@users.noreply.github.com>
2022-09-01 02:30:54 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-su root
-apt-get update && apt-get install sudo -y
-sudo apt-get install build-essential libssl-dev -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
-git clone https://gitlab.com/gemilanga163/onta.git && cd onta
-chmod u+x gas && ./gas

